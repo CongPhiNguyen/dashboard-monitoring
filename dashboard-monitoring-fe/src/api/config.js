f34b2812c6d037eb82560c6f17e8c0b7cff1ce23
CongPhiNguyen <congphinguyen312@gmail.com>
2022-10-03 10:26:30 +0700

    fix api

--- dashboard-monitoring-fe/src/api/config.js
+++ dashboard-monitoring-fe/src/api/config.js
@@ -3,9 +3,9 @@ const URL = {
   URL_GET_ALL_DATA: URL_SYSTEM_V1 + "/vui-coins/get-all-data",
   URL_ADMIN_LOGIN: URL_SYSTEM_V1 + "/user/login",
   URL_ADMIN_REFRESH: URL_SYSTEM_V1 + "/user/refresh",
-  URL_GET_DATA_WEEK: URL_SYSTEM_V1 + "/vui-coins/get-data-week",
-  URL_GET_DATA_DAY: URL_SYSTEM_V1 + "/vui-coins/get-data-day",
-  URL_GET_DATA_HOURS: URL_SYSTEM_V1 + "/vui-coins/get-data-hours",
+  URL_GET_DATA_WEEK: URL_SYSTEM_V1 + "/vui-coins/get-data-by-week",
+  URL_GET_DATA_DAY: URL_SYSTEM_V1 + "/vui-coins/get-data-by-day",
+  URL_GET_DATA_HOURS: URL_SYSTEM_V1 + "/vui-coins/get-data-by-hours",
 };
 
 export default URL;
