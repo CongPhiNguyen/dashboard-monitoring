fe0bdc8c047990a5cb3d4386b6c97afd63077a7a
Luong Thien Phuoc <phuoc.t.luong@taptap.com.vn>
2022-10-06 13:12:23 +0700

    done

--- dashboard-monitoring-be/src/routers/vuiCoins.js
+++ dashboard-monitoring-be/src/routers/vuiCoins.js
@@ -1,18 +1,36 @@
 const express = require("express")
+
 const router = express.Router()
+
 const vuiCoinsController = require("../controllers/vuiCoinsController")
 
+
+
 router.get("/get-all-data", vuiCoinsController.getAllDataVui)
+
 router.get("/get-data-by-week", vuiCoinsController.getDataByWeek)
+
 router.get("/get-data-by-day", vuiCoinsController.getDataByDay)
+
 router.get("/get-data-by-hour", vuiCoinsController.getDataByHour)
+
 router.get("/get-data-by-minute", vuiCoinsController.getDataByMinute)
+
 router.get("/get-concrete-value", vuiCoinsController.getConcreteValue)
+
 router.get("/get-concrete-data", vuiCoinsController.getConcreteData)
+
+router.get("/get-concrete-data-2", vuiCoinsController.getConcreteData2)
+
 router.get("/get-transaction", vuiCoinsController.getTransactionDataInMinute)
+
 router.get(
   "/get-concrete-transaction",
   vuiCoinsController.getConcreteTransactionData
+)
+router.get(
+  "/get-concrete-transaction-2",
+  vuiCoinsController.getConcreteTransactionData2
 )
 
 module.exports = router
